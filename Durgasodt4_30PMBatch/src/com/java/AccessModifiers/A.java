2c8a15854eb055977e918680c5a48781fc0cf4d5
ravilella2018 <ravilella.2018@gmail.com>
2018-11-02 17:31:10 +0530

    Static Instance

--- Durgasodt4_30PMBatch/src/com/java/AccessModifiers/A.java
+++ Durgasodt4_30PMBatch/src/com/java/AccessModifiers/A.java
@@ -2,19 +2,31 @@ package com.java.AccessModifiers;
 
 public  class A 
 {
+	
+	
 	int x=100; // instance (or) global (or) non-static variable
 	static float roi= 8.5f; // static variable
+	static A a;
 	
 	public void m1()
 	{
 		int x=200;
 		System.out.println("iam m1 in class A");
-		System.out.println(this.x);
+		System.out.println(x);
+		System.out.println(A.roi);
+	}
+	
+	public static void m5()
+	{
+		a=new A();
+		System.out.println(a.x);
+		System.out.println(A.roi);
+		
 	}
 	
 	public static void main(String[] args) 
 	{
-		A a=new A();
+		a=new A();
 		a.m1();
 		System.out.println(a.x);
 		System.out.println(A.roi);
